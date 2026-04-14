3ca7b1496d83812f0807eb12d75f616c389979dc
SamuelRamirezM <156239082+SRamirez967@users.noreply.github.com>
2026-04-14 16:06:14 -0500

    Modo oscuro

--- registro.js
+++ registro.js
@@ -4,15 +4,29 @@ const ventanaIngresar = document.getElementById("ventanaIngresar");
 function mostrarVentanaIngreso() {
     const ingreso = document.getElementById("ingreso");
     const registro = document.getElementById("registro");
+
     ingreso.classList.remove("hidden");
     registro.classList.add("hidden");
+
+    ventanaIngresar.classList.add("border-b-2");
+    ventanaIngresar.classList.add("border-[#06D6A0]");
+
+    ventanaRegistrarse.classList.remove("border-b-2");
+    ventanaRegistrarse.classList.remove("border-[#06D6A0]");
 }
 
 function mostrarVentanaRegistro() {
     const ingreso = document.getElementById("ingreso");
     const registro = document.getElementById("registro");
+
     ingreso.classList.add("hidden");
     registro.classList.remove("hidden");
+
+    ventanaRegistrarse.classList.add("border-b-2");
+    ventanaRegistrarse.classList.add("border-[#06D6A0]");
+
+    ventanaIngresar.classList.remove("border-b-2");
+    ventanaIngresar.classList.remove("border-[#06D6A0]");
 }
 
 ventanaRegistrarse.addEventListener("click", mostrarVentanaRegistro);
