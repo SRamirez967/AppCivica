9e2e65b9b5a6ff44bb607a1afd8b8a358fbbf9b3
SamuelRamirezM <156239082+SRamirez967@users.noreply.github.com>
2026-04-16 15:59:21 -0500

    Creacion de clase Usuario

--- index.js
+++ index.js
@@ -40,15 +40,54 @@ function realizarRecarga(callback) {
 
 }
 
+// Toggle sidebar
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+
+    sidebar.classList.toggle("hidden");
+}
+
 // Aquí vamos a comenzar a crear los usuarios a partir de una clase.
+let listaUsuarios = [];
 
 class Usuario {
 
-    constructor(nombre, tipoDocumento, numeroDocumento, correo) {
+    constructor(nombre, tipoDocumento, numeroDocumento, correo, clave) {
+        // Datos con los que el usuario se registra
         this.nombre = nombre;
         this.tipoDocumento = tipoDocumento;
         this.numeroDocumento = numeroDocumento;
         this.correo = correo;
+        this.clave = clave;
+
+        // Datos adicionales que el usuario llena (opcionalmente pero recomendado)
+        this.numeroCelular;
+        this.telefonoContacto;
+        this.ocupacion;
+        this.direccion;
+
+        // Datos de la Civica
+        this.saldoCivica = 0;
+        this.perfilCivica;
+        this.numeroTarjeta;
+        this.registroMovimientos = [];
+        this.saldoPendiente = false;
+
+        // Datos de CivicaPay
+        this.saldoCivicaPay = 0;
+        
     }
 }
+
+const nombre = window.prompt("Ingrese su nombre:");
+const tipoDocumento = window.prompt("Ingrese su tipo de documento:");
+const numeroDocumento = window.prompt("Ingrese su numero de documento:");
+const correo = window.prompt("Ingrese su correo:");
+const clave = window.prompt("Ingrese su clave:");
+
+const user1 = new Usuario(nombre, tipoDocumento, numeroDocumento, correo, clave)
+
+const bienvenida = document.getElementById("bienvenida");
+bienvenida.textContent = `Bienvenido de nuevo, ${user1.nombre}!`
 
