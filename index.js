681c77263adc371cabe2a5b6a10a7e90887504f0
SamuelRamirezM <156239082+SRamirez967@users.noreply.github.com>
2026-05-01 15:57:13 -0500

    Algo de logica de usuario para registrarse

--- index.js
+++ index.js
@@ -50,3 +50,98 @@ function abrirVentanaRecargaExitosa() {
 function cerrarVentanaRecargaExitosa() {
     ventanaRecargaExitosa.classList.add("hidden");
 }
+
+// Lógica de creación de usuarios
+
+class User {
+    static usuarios = [];
+
+    constructor (nombre, tipo_documento, numero_documento, correo, clave) {
+        this.nombre = nombre;
+        this.tipo_documento = tipo_documento;
+        this.numero_documento = numero_documento;
+        this.correo = correo;
+        this.clave = clave;
+        this.saldo_civica = 0;
+        this.saldo_civicapay = 0;
+        this.historial_pagos = [];
+    }
+}
+
+function flujoPrincipal() {
+    console.log("Bienvenido! ¿Deseas registrarte o ingresar?");
+    console.log("1. Registrarme");
+    console.log("2. Ingresar");
+
+    const opcion = window.prompt("Ingresa una opción: ");
+
+    let usuario;
+
+    switch (opcion) {
+        case "1":
+            usuario = registrarse();
+            break;
+        case "2":
+            usuario = ingresar();
+            break
+    }
+
+    const bienvenida = document.getElementById("bienvenida");
+    const mensajeSaldo = document.getElementById("saldo");
+
+    bienvenida.textContent = `Bienvenido de nuevo, ${usuario.nombre}!`;
+    mensajeSaldo.textContent = `$${usuario.saldo_civica}`;
+
+
+}
+
+function registrarse() {
+    const nombre = window.prompt("Ingrese su nombre: ");
+    let tipo_documento = window.prompt("Ingrese su tipo de documento: 1. Tarjeta de identidad, 2. Cédula, 3. Cédula extranjera: ");
+    switch (tipo_documento) {
+        case "1":
+            tipo_documento = "Tarjeta de identidad";
+            break;
+        case "2":
+            tipo_documento = "Cédula";
+            break;
+        case "3":
+            tipo_documento = "Cédula extranjera";
+            break;
+    }
+    const numero_documento = window.prompt("Ingrese su numero de documento: ");
+    const correo = window.prompt("Ingrese su correo electrónico: ");
+    const clave = window.prompt("Ingrese una clave: ");
+
+    const usuario = new User(nombre, tipo_documento, numero_documento, correo, clave);
+    User.usuarios.push(usuario);
+
+    return usuario;
+}
+
+function ingresar() {
+    let tipo_documento = window.prompt("Ingrese su tipo de documento: 1. Tarjeta de identidad, 2. Cédula, 3. Cédula extranjera: ");
+    switch (tipo_documento) {
+        case "1":
+            tipo_documento = "Tarjeta de identidad";
+            break;
+        case "2":
+            tipo_documento = "Cédula";
+            break;
+        case "3":
+            tipo_documento = "Cédula extranjera";
+            break;
+    }
+    const numero_documento = window.prompt("Ingrese su numero de documento: ");
+    const clave = window.prompt("Ingrese una clave: ");
+
+    for (user of User.usuarios) {
+        if (user.numero_documento === numero_documento) {
+            return user;
+        } 
+    }
+
+    console.log("No se encontró el usuario. Por favor registrese.");
+}
+
+flujoPrincipal();
